acc0c6f7a373c3a0f867339d423565319964ad30
alfandi2020 <pandibl4530@gmail.com>
2022-08-22 11:13:51 +0700

    a

--- application/views/body/laporan.php
+++ application/views/body/laporan.php
@@ -39,7 +39,7 @@
                         if ($agent_get == true) { 
                             $get_saldo = $this->db->query("SELECT * FROM dt_agent where id_agent='$agent_get'")->row_array();
                             ?>
-                            <h6>Saldo Terakhir : Rp.<?= number_format($get_saldo['saldo'],0,".",".") ?></h6>
+                            <h6>Saldo akhir : Rp.<?= number_format($get_saldo['saldo'],0,".",".") ?></h6>
                         <?php } ?>
                         <div class="table-responsive">
                             <table class="table zero-configuration" >
