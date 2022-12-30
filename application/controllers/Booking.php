5a4ef021467e57e47ddbd91747385a9ddaa99bc7
alfandi2020 <pandibl4530@gmail.com>
2022-12-30 10:23:58 +0700

    aa

--- application/controllers/Booking.php
+++ application/controllers/Booking.php
@@ -1,7 +1,7 @@
 <?php
 
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Pricelist extends CI_Controller {
+class Booking extends CI_Controller {
 
     public function  __construct()
     {
